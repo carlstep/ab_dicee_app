232af972c1f4ee46f44f30050f95cc5c84010033
carlstep <carlstep333@me.com>
2021-05-18 20:24:45 +0200

    created 5 dice, grey design

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,12 @@ import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.grey[800],
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.grey[900],
         ),
         body: DicePage(), // gives a StatelessWidget >> see DicePage class
       ),
@@ -22,39 +23,72 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 5; // creates the variable for the left dice
-  int rightDiceNumber = 1; // creates the variable for the right dice
+  int diceNumberOne = 5; // creates the variable for the left dice
+  int diceNumberTwo = 1;
+  int diceNumberThree = 1;
+  int diceNumberFour = 2;
+  int diceNumberFive = 3; // creates the variable for the right dice
 
   void changeDiceFace() {
     setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber =
+      diceNumberOne = Random().nextInt(6) + 1;
+      diceNumberTwo = Random().nextInt(6) + 1;
+      diceNumberThree = Random().nextInt(6) + 1;
+      diceNumberFour = Random().nextInt(6) + 1;
+      diceNumberFive =
           Random().nextInt(6) + 1; // changes the left dice variable value
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                changeDiceFace();
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$diceNumberOne.png'),
+              ),
             ),
-          ),
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                changeDiceFace();
-              },
-              child: Image.asset('images/dice$rightDiceNumber.png'),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$diceNumberTwo.png'),
+              ),
             ),
-          )
-        ],
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$diceNumberThree.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$diceNumberFour.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$diceNumberFive.png'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
